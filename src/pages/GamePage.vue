0dda1a07e9594e379285c3bc6c1e1530abeeff17
ceroglu00 <c.eroglu00@gmail.com>
2026-02-27 14:39:10 +0300

    ++

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,20 +1,37 @@
 <template>
+  <v-container fluid class="pa-2">
 
-  <div v-for="(row,x) in 3">
-    <div v-for="(col,y) in 3">
+    <v-row>
+      <v-col class="v-col-4 pa-0" v-for="(cell,index) in 9">
+        <div class="
+        d-flex
+      bg-blue
+      justify-center
+      align-center
+      border-solid
+"
+             @click="checkCell(index)"
+        >
+          {{ board[index] }}
+        </div>
+      </v-col>
+    </v-row>
 
-<!--      {{ x }}-{{ y }}-->
+  </v-container>
 
-      {{asdasd}}
-
-    </div>
-  </div>
 
 </template>
 
 <script setup>
 
-var board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+import {ref} from "vue";
+
+var board = ref([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+
+function checkCell(index) {
+  board.value[index] = "X"
+}
 
 </script>
 
