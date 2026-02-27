c8a7562c858d046a03f8213b9b6724aeb0844225
ceroglu00 <c.eroglu00@gmail.com>
2026-02-27 14:06:12 +0300

    ++

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,10 +1,20 @@
 <template>
 
+  <div v-for="(row,x) in 3">
+    <div v-for="(col,y) in 3">
 
+<!--      {{ x }}-{{ y }}-->
+
+      {{asdasd}}
+
+    </div>
+  </div>
 
 </template>
 
 <script setup>
+
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 </script>
 
